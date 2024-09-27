3fe891e8946cd7173178baedc73fcef4f12a5b86
m4rk-b <128698390+m4rk-b@users.noreply.github.com>
2024-09-27 10:43:59 +0800

    Update API path on test_db.ts.

--- test_db.ts
+++ test_db.ts
@@ -4,7 +4,7 @@ export async function bidding(
     itemid:number, sessionid:number, bidamount:number
   ) {
     try {
-      const res = await fetch(`http://localhost:10000/bid`, {
+      const res = await fetch(`${process.env.API_PATH}/bid`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
