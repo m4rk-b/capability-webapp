8a835a12d861f42db3dc767f2fe127903fa8c697
Mark Gil Bejasa <bejasa.markgilc@gmail.com>
2024-10-07 09:21:56 +0800

    Update UI.

--- app/home/(navbar)/(components)/nav-menu.tsx
+++ app/home/(navbar)/(components)/nav-menu.tsx
@@ -21,7 +21,7 @@ function NavMenu() {
               />
             </svg>
           </div>
-          <ul
+          {/* <ul
             tabIndex={0}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
           >
@@ -34,7 +34,7 @@ function NavMenu() {
             <li>
               <a>About</a>
             </li>
-          </ul>
+          </ul> */}
         </div>
       </div>
     </>
